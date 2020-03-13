b206893c34985cf33f44da288863886afcea5b87
Tom McKeesick <tmck01@gmail.com>
2020-03-13 16:58:51 +1100

    Separate building cowfiles and go binary

--- install.sh
+++ install.sh
@@ -5,30 +5,24 @@ pokesay_bin="pokesay"
 install_path="$HOME/.${pokesay_bin}"
 bin_path="$HOME/bin"
 
-# Make sure the directories exist
-mkdir -p $install_path/
-mkdir -p $install_path/cows/
-mkdir -p $bin_path/
-
-if [ ! -f ${bin_path}/cowsay ]; then
-  go get -u -v github.com/msmith491/go-cowsay/...
-  ln -sv $HOME/go/src/github.com/msmith491/go-cowsay/cowsay $HOME/bin/
-fi
-
-# Remove any previously installed cows
-rm -rf $install_path/cows/*
+# Make sure the install paths exist
+mkdir -p ${install_path}/cows/ ${bin_path}
+# Remove any previously installed cowfiles and binaries
+rm -rf ${install_path}/cows/* ${bin_path}/cowsay
 
 # Copy the cows and the main script to the install path.
-tar xzf cows.tar.gz -C $install_path/
-N_POKEMON=$(find $install_path/ -type f -name *.cow | wc -l)
-cp $pokesay_bin $bin_path/$pokesay_bin
-chmod +x "$bin_path/$pokesay_bin"
-echo "\nCopied $N_POKEMON Pokémon to install path '$install_path'"
+tar xzf cows.tar.gz -C ${install_path}/
+N_POKEMON=$(find ${install_path}/ -type f -name *.cow | wc -l)
+# Copy the executable to the install path and ensure it has +x permissions
+cp -v ${pokesay_bin} cowsay ${bin_path}/
+chmod +x "${bin_path}/${pokesay_bin}"
 
-echo "\
+cat <<EOF
 
-- The files were installed to '$install_path/'.
-- A '$pokesay_bin' script was created in '$bin_path/'.
+Copied $N_POKEMON Pokémon to install path '${install_path}'
 
-It may be necessary to logout and login back again in order to have the '$pokesay_bin' available in your path.
-"
+- The files were installed to '${install_path}/'.
+- A '${pokesay_bin}' script was created in '${bin_path}/'.
+
+It may be necessary to logout and login back again in order to have the '${pokesay_bin}' available in your path.
+EOF
