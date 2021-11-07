dd4b9de4694f773df6574df64cffb216189bba8a
Tom McKeesick <tmck01@gmail.com>
2021-11-07 20:52:44 +1100

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+VERSION=0.1.0
+GOOS=$1
+GOARCH=$2
+
+mkdir -p $HOME/bin
+curl -L --output $HOME/bin/pokesay https://github.com/tmck-code/pokesay-go/releases/download/v${VERSION}/pokesay-$GOOS-$GOARCH
