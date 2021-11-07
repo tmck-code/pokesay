5419605f452035ba2112e73cc906617795413e9b
Tom McKeesick <tmck01@gmail.com>
2021-11-07 21:02:44 +1100

    Add a demo to the installer

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ GOARCH=$2
 
 mkdir -p $HOME/bin
 curl -L --output $HOME/bin/pokesay https://github.com/tmck-code/pokesay-go/releases/download/v${VERSION}/pokesay-$GOOS-$GOARCH
+export PATH="$HOME/bin:$PATH"
+echo "hello world!" | pokesay
