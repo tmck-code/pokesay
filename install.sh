90ba4ba583124fbbb6350edfb3c1a8acbc3f54f2
Tom McKeesick <tmck01@gmail.com>
2021-11-08 09:55:34 +1100

    update

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ GOOS=$1
 GOARCH=$2
 
 mkdir -p $HOME/bin
+sudo chown -R $USER:$USER $HOME/bin
 curl -L --output $HOME/bin/pokesay https://github.com/tmck-code/pokesay-go/releases/download/v${VERSION}/pokesay-$GOOS-$GOARCH
 export PATH="$HOME/bin:$PATH"
 echo "hello world!" | pokesay
