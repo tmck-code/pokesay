06888216950b071eb845bb2786cad461474ebdd4
Tom McKeesick <tmck01@gmail.com>
2021-11-08 19:20:21 +1100

    update cow conversion progress bar

--- build/build_cows.sh
+++ build/build_cows.sh
@@ -14,24 +14,9 @@ function generateCowfile() {
   local local_path=${ofpath##*icons/}
   local dir_path=$(dirname /tmp/cows/${local_path})
 
-  # Cowsay bubble header
-#   cat <<EOF > "${ofpath}"
-# binmode STDOUT, ":utf8";
-# \$the_cow =<<EOC;
-#      \$thoughts
-#       \$thoughts
-#        \$thoughts
-#         \$thoughts
-# EOF
-
   # Pokemon
   img2xterm "${f}" | sed '/^\ $/d' >> "${ofpath}"
   echo -e "\n" >> "{ofpath}"
-
-  # Cowsay footer
-#   cat <<EOF >> "${ofpath}"
-# EOC
-# EOF
 
   [ -d "${dir_path}" ] || mkdir -p "${dir_path}"
   mv ${ofpath} /tmp/cows/${local_path}
@@ -42,8 +27,8 @@ find ${DOCKER_BUILD_DIR} -type d | parallel -I {} mkdir -p "${DOCKER_OUTPUT_DIR}
 total=$(find ${DOCKER_BUILD_DIR} -type f -iname *.png | wc -l)
 
 
-BAR='▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇' # this is full bar
-EMP='                             ' # This is an empty bar
+BAR='▇▇▇▇▇▇▇▇▇▇' # this is full bar
+EMP='----------' # This is an empty bar
 
 for i in {1..20}; do
     sleep .1                 # wait 100ms between "frames"
@@ -55,8 +40,7 @@ for f in $(find ${DOCKER_BUILD_DIR} -type f -name *.png); do
   generateCowfile "${f}" &
   if ! (( i % 10 )); then
     index=$[ i / 100 ]
-    end=$[ 30 - $[ i / 100 ] ]
-    echo -ne "\r${BAR:0:$index}${EMP:0:$end}" # print $i chars of $BAR from 0 position
+    echo -ne "\r $i ${BAR:0:$index}${EMP:$index:10}" # print $i chars of $BAR from 0 position
   fi
   ((i=i+1))
 done
