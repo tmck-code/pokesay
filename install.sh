ae33f9f6e193f41744b4c75aae81adf53e3d48bc
Tom McKeesick <tmck01@gmail.com>
2021-11-08 12:36:57 +1100

    deal with termux installation

--- install.sh
+++ install.sh
@@ -7,11 +7,16 @@ GOOS=$1
 GOARCH=$2
 
 mkdir -p $HOME/bin
-
 curl -sL -o pokesay https://github.com/tmck-code/pokesay-go/releases/download/v${VERSION}/pokesay-$GOOS-$GOARCH
-# Use sudo in case someone's $HOME/bin dir is root-owned
-sudo mv -v ./pokesay $HOME/bin/
-sudo chmod u+wrx $HOME/bin/pokesay
+
+if [ "${TERMUX:-}" == 1 ]; then
+  mv -v ./pokesay $HOME/bin/
+  chmod u+wrx $HOME/bin/pokesay
+else
+  # Use sudo in case $HOME/bin is root-owned
+  sudo mv -v ./pokesay $HOME/bin/
+  sudo chmod u+wrx $HOME/bin/pokesay
+fi
 
 export PATH="$HOME/bin:$PATH"
 echo "hello world!" | pokesay
