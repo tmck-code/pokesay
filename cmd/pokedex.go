7004527040f2ffda8e6c17ab66ea80205615260c
Tom McKeesick <tmck01@gmail.com>
2022-03-15 21:10:20 +1100

    update
    Write categories and pokemon to file as a map of categories -> pokemon
    entry

--- cmd/pokedex.go
+++ cmd/pokedex.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-    "bufio"
+	"bufio"
+	"encoding/gob"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
-	"encoding/gob"
+	"time"
 )
 
 type PokemonEntry struct {
@@ -18,8 +20,8 @@ type PokemonEntry struct {
 	Categories []string
 }
 
-func findFiles(dirpath string, ext string, skip []string) []*PokemonEntry {
-	pokedex := []*PokemonEntry{}
+func findFiles(dirpath string, ext string, skip []string) map[string][]PokemonEntry {
+	categories := make(map[string][]PokemonEntry)
 	err := filepath.Walk(dirpath, func(fpath string, f os.FileInfo, err error) error {
 		for _, s := range skip {
 			if strings.Contains(fpath, s) {
@@ -27,20 +29,23 @@ func findFiles(dirpath string, ext string, skip []string) []*PokemonEntry {
 			}
 		}
 		if !f.IsDir() && filepath.Ext(f.Name()) == ext {
-			data, _ := os.ReadFile(fpath)
+			data, err := os.ReadFile(fpath)
+			check(err)
+			pokemonCategories := createCategories(fpath)
+			for _, c := range pokemonCategories[3 : len(pokemonCategories)-1] {
 
-			pokedex = append(
-				pokedex,
-				&PokemonEntry{Name: fpath, Data: data, },
-			)
+				if val, ok := categories[c]; ok {
+					categories[c] = append(val, PokemonEntry{Name: fpath, Data: data})
+				} else {
+					categories[c] = []PokemonEntry{PokemonEntry{Name: fpath, Data: data}}
+				}
+			}
 		}
 		return err
 	})
-	if err != nil {
-		fmt.Println("Fatal!")
-		log.Fatal(err)
-	}
-	return pokedex
+	check(err)
+
+	return categories
 }
 
 func createCategories(fpath string) []string {
@@ -55,18 +60,39 @@ func createPokemonEntry(pokemon *PokemonEntry, fpath string) *PokemonEntry {
 	}
 }
 
-func writeToFile(pokemon []*PokemonEntry, fpath string) {
-    ostream, err := os.Create(fpath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    writer := bufio.NewWriter(ostream)
-    enc := gob.NewEncoder(writer)
-    enc.Encode(pokemon)
-    writer.Flush()
-    ostream.Close()
-	
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func writeToFile(categories map[string][]PokemonEntry, fpath string) {
+	ostream, err := os.Create(fpath)
+	check(err)
+
+	writer := bufio.NewWriter(ostream)
+	enc := gob.NewEncoder(writer)
+	enc.Encode(categories)
+	writer.Flush()
+	ostream.Close()
+
 	fmt.Println("->", fpath)
+}
+
+func readFromFile(fpath string) map[string][]PokemonEntry {
+	istream, err := os.Open(fpath)
+	check(err)
+
+	reader := bufio.NewReader(istream)
+	dec := gob.NewDecoder(reader)
+
+	categories := make(map[string][]PokemonEntry)
+
+	err = dec.Decode(&categories)
+	check(err)
+	istream.Close()
+
+	return categories
 }
 
 type CowBuildArgs struct {
@@ -88,12 +114,37 @@ func parseArgs() CowBuildArgs {
 	return args
 }
 
+func randInt(n int) int {
+	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(n)
+}
+
 func main() {
 	args := parseArgs()
 	fmt.Println("starting at", args.FromDir)
 
-	pokedex := findFiles(args.FromDir, ".png", args.SkipDirs)
+	// categories := findFiles(args.FromDir, ".cow", args.SkipDirs)
 
-	writeToFile(pokedex, args.ToFpath)
+	// writeToFile(categories, args.ToFpath)
+
+	categories := readFromFile(args.ToFpath)
+
+	total := 0
+	for _, _ = range categories {
+		total += 1
+	}
+	randomCategory := randInt(total)
+
+	idx := 0
+	choice := ""
+	for cat, _ := range categories {
+		idx += 1
+		if idx == randomCategory {
+			choice = cat
+		}
+	}
+	fmt.Println("category choice", choice)
+
+	pokemon := categories[choice][randInt(len(categories[choice]))]
+
+	fmt.Printf("%s\n", pokemon.Data)
 }
-
