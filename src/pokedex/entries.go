df36431cff4bf23e566e707835684f3109e85811
Tom McKeesick <tmck01@gmail.com>
2022-05-17 19:58:58 +1000

    Add extra error guards and checks

--- src/pokedex/entries.go
+++ src/pokedex/entries.go
@@ -128,15 +128,21 @@ func (t PokemonTrie) GetCategoryPaths(s string) ([][]string, error) {
 func (t PokemonTrie) GetCategory(s []string) ([]*PokemonEntry, error) {
 	current := t.Root
 	matches := make([]*PokemonEntry, 0)
+	found := false
 	for _, char := range s {
-		if _, ok := current.Children[char]; ok {
+		if _, ok := current.Children[char]; ok || found {
+			found = true
 			current = current.Children[char]
 			for _, p := range current.Children {
+				fmt.Println(p)
 				matches = append(matches, p.Data...)
 			}
 		} else {
 			return nil, errors.New(fmt.Sprintf("Could not find category: %s", s))
 		}
+	}
+	if len(matches) == 0 {
+		return nil, errors.New(fmt.Sprintf("Could not find category: %s", s))
 	}
 	return matches, nil
 }
