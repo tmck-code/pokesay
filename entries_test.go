62031b24558db1193e4ab1504a75fc59825704c8
Tom McKeesick <tmck01@gmail.com>
2022-05-17 20:15:29 +1000

    some test updates

--- entries_test.go
+++ entries_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func assert(expected interface{}, result interface{}, obj interface{}, test *testing.T) {
-	if expected != result {
+	if fmt.Sprintf("%v", expected) != fmt.Sprintf("%v", result) {
 		test.Fatalf(`expected = %+v, result = %+v, obj = %+v`, expected, result, obj)
 	}
 }
@@ -38,24 +38,39 @@ func TestTrieInsert(test *testing.T) {
 		result, test,
 	)
 }
-func TestTrieInsert2(test *testing.T) {
+func TestCategoryPaths(test *testing.T) {
 	t := pokedex.NewTrie()
 	t.Insert(
-		[]string{"pokemon", "gen1", "regular"},
+		[]string{"small", "gen1", "regular"},
 		pokedex.NewPokemonEntry(0, "pikachu"),
 	)
 	t.Insert(
-		[]string{"pokemon", "gen1", "other"},
+		[]string{"small", "gen1", "other"},
 		pokedex.NewPokemonEntry(1, "bulbasaur"),
 	)
+	t.Insert(
+		[]string{"medium", "gen1", "other"},
+		pokedex.NewPokemonEntry(2, "bulbasaur"),
+	)
+	t.Insert(
+		[]string{"big", "gen1", "other"},
+		pokedex.NewPokemonEntry(3, "bulbasaur"),
+	)
+	t.Insert(
+		[]string{"big", "gen1"},
+		pokedex.NewPokemonEntry(4, "charmander"),
+	)
 
-	result, err := t.GetCategory([]string{"pokemon", "gen1"})
+	result, err := t.GetCategoryPaths("big")
 	check(err)
 
-	assert(2, len(result), result, test)
-	assert(
-		"[{Index: 0, Name: pikachu} {Index: 1, Name: bulbasaur}]",
-		fmt.Sprintf("%s", result),
-		result, test,
-	)
+	expected := [][]string{
+		[]string{"small", "gen1", "regular"},
+		[]string{"small", "gen1", "other"},
+		[]string{"medium", "gen1", "other"},
+		[]string{"big", "gen1", "other"},
+		[]string{"big", "gen1"},
+	}
+	assert(expected, t.Keys, t, test)
+	assert("", result, result, test)
 }
