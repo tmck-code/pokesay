4b4e720e5b46a6ee0ce773264224d40b9afbb0d1
Tom McKeesick <tmck01@gmail.com>
2022-05-17 20:01:45 +1000

    Add a basic unit test file
    There's some weirdness going on in the Trie selection, going to make
    unit tests to make everything rock-solid

---
+++ entries_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+	"github.com/tmck-code/pokesay/src/pokedex"
+)
+
+func assert(expected interface{}, result interface{}, obj interface{}, test *testing.T) {
+	if expected != result {
+		test.Fatalf(`expected = %+v, result = %+v, obj = %+v`, expected, result, obj)
+	}
+}
+
+func TestNewPokemonEntry(test *testing.T) {
+	p := pokedex.NewPokemonEntry(1, "yo")
+	assert(1, p.Index, p, test)
+}
+
+func TestTrieInsert(test *testing.T) {
+	t := pokedex.NewTrie()
+	t.Insert(
+		[]string{"pokemon", "gen1", "regular"},
+		pokedex.NewPokemonEntry(0, "pikachu"),
+	)
+	t.Insert(
+		[]string{"pokemon", "gen1", "regular"},
+		pokedex.NewPokemonEntry(1, "bulbasaur"),
+	)
+
+	result, err := t.GetCategory([]string{"pokemon", "gen1"})
+	check(err)
+
+	assert(2, len(result), result, test)
+	assert(
+		"[{Index: 0, Name: pikachu} {Index: 1, Name: bulbasaur}]",
+		fmt.Sprintf("%s", result),
+		result, test,
+	)
+}
+func TestTrieInsert2(test *testing.T) {
+	t := pokedex.NewTrie()
+	t.Insert(
+		[]string{"pokemon", "gen1", "regular"},
+		pokedex.NewPokemonEntry(0, "pikachu"),
+	)
+	t.Insert(
+		[]string{"pokemon", "gen1", "other"},
+		pokedex.NewPokemonEntry(1, "bulbasaur"),
+	)
+
+	result, err := t.GetCategory([]string{"pokemon", "gen1"})
+	check(err)
+
+	assert(2, len(result), result, test)
+	assert(
+		"[{Index: 0, Name: pikachu} {Index: 1, Name: bulbasaur}]",
+		fmt.Sprintf("%s", result),
+		result, test,
+	)
+}
