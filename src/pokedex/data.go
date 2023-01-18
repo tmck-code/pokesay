2581ed957e06f0bc4dc760ec5113cf4296623a28
Tom McKeesick <tmck01@gmail.com>
2023-01-18 17:49:02 +1100

    gofmt update

--- src/pokedex/data.go
+++ src/pokedex/data.go
@@ -1,28 +1,29 @@
 package pokedex
 
 import (
-	"os"
-	"log"
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
-// {
-//   "name": { "eng": "Charizard", "chs": "喷火龙", "jpn": "リザードン", "jpn_ro": "Lizardon" }
-//   "slug": { "eng": "charizard",                  "jpn": "riza-don",   "jpn_ro": "lizardon" }
-// }
+//	{
+//	  "name": { "eng": "Charizard", "chs": "喷火龙", "jpn": "リザードン", "jpn_ro": "Lizardon" }
+//	  "slug": { "eng": "charizard",                  "jpn": "riza-don",   "jpn_ro": "lizardon" }
+//	}
+//
 // Out of all these names, we want the name.jpn, name.jpn_ro, slug.eng,
 type DataEntryName struct {
-	Eng string `json:"eng"`
-	Jpn string `json:"jpn"`
+	Eng    string `json:"eng"`
+	Jpn    string `json:"jpn"`
 	Jpn_ro string `json:"jpn_ro"`
 }
 
 type DataEntrySlug struct {
-	Eng string `json:"eng"`
-	Jpn string `json:"jpn"`
+	Eng    string `json:"eng"`
+	Jpn    string `json:"jpn"`
 	Jpn_ro string `json:"jpn_ro"`
 }
 
@@ -32,42 +33,40 @@ type DataEntry struct {
 }
 
 type PokemonName struct {
-	English string
-	Japanese string
+	English          string
+	Japanese         string
 	JapanesePhonetic string
 }
 
-
 func NewPokemonName(entry DataEntry) *PokemonName {
 	return &PokemonName{
-		English: entry.Name.Eng,
-		Japanese: entry.Name.Jpn,
+		English:          entry.Name.Eng,
+		Japanese:         entry.Name.Jpn,
 		JapanesePhonetic: entry.Slug.Jpn,
 	}
 }
 
 func ReadNames(fpath string) map[string]PokemonName {
-    istream, err := os.Open(fpath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer istream.Close()
+	istream, err := os.Open(fpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer istream.Close()
 
 	entries := make(map[string]PokemonName)
-    scanner := bufio.NewScanner(istream)
-    // optionally, resize scanner's capacity for lines over 64K, see next example
-    for scanner.Scan() {
+	scanner := bufio.NewScanner(istream)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
 		line := scanner.Bytes()
-	    var entry DataEntry
+		var entry DataEntry
 		jsonErr := json.Unmarshal(line, &entry)
 		if jsonErr != nil {
 			fmt.Println(jsonErr)
 		}
 		entries[strings.ToLower(entry.Name.Eng)] = *NewPokemonName(entry)
-    }
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 	return entries
 }
-
