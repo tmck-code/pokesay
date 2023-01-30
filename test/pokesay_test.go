5fd41c8c7aac35ab955e11e3de92878bd38cc190
Tom McKeesick <tmck01@gmail.com>
2023-01-30 21:08:47 +1100

    Test choosing random category
    Bonus, add an AssertContains templated (woo!) function to help with
    asserting if an item is contained in a slice

--- test/pokesay_test.go
+++ test/pokesay_test.go
@@ -18,5 +18,29 @@ func TestListCategories(test *testing.T) {
 	result := pokesay.ListCategories(*t)
 	expected := []string{"big", "g1", "medium", "o", "r", "small"}
 
-	Assert(expected, result, result, test)
+	Assert(expected, result, test)
+}
+
+func TestChooseRandomCategory(test *testing.T) {
+	t := pokedex.NewTrie()
+	t.Insert([]string{"small", "g1", "r"}, pokedex.NewEntry(0, "pikachu"))
+	t.Insert([]string{"small", "g1", "o"}, pokedex.NewEntry(1, "bulbasaur"))
+	t.Insert([]string{"medium", "g1", "o"}, pokedex.NewEntry(2, "bulbasaur"))
+	t.Insert([]string{"big", "g1", "o"}, pokedex.NewEntry(3, "bulbasaur"))
+	t.Insert([]string{"big", "g1"}, pokedex.NewEntry(4, "charmander"))
+
+	resultKeys, err := t.FindKeyPaths("big")
+	pokesay.Check(err)
+
+	expectedKeys := [][]string{{"big", "g1", "o"}, {"big", "g1"}}
+	Assert(expectedKeys, resultKeys, test)
+
+	resultKey, result := pokesay.ChooseRandomCategory(resultKeys, *t)
+	AssertContains(expectedKeys, resultKey, test)
+
+	expectedResult := []pokedex.Entry{
+		{Index: 3, Value: "bulbasaur"},
+		{Index: 4, Value: "charmander"},
+	}
+	AssertContains(expectedResult, *result[0], test)
 }
