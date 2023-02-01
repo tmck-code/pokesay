7ec0a564c7fd183fe438611bd5ec5e3d395f9f40
Tom McKeesick <tmck01@gmail.com>
2023-02-01 19:07:38 +1100

    Write each cowfile to embedded FS

--- src/bin/pokedex/pokedex.go
+++ src/bin/pokedex/pokedex.go
@@ -8,6 +8,7 @@ import (
 	"path"
 	"strings"
 
+	"github.com/tmck-code/pokesay/src/bin"
 	"github.com/tmck-code/pokesay/src/pokedex"
 )
 
@@ -123,15 +124,17 @@ func main() {
 		categoryFpath,
 	)
 
-	// fmt.Println("- Writing metadata and entries to file")
-	// pbar := bin.NewProgressBar(len(metadata))
-	// for _, m := range metadata {
-	// 	pokedex.WriteBytesToFile(m.Data, pokedex.EntryFpath(entryDirPath, m.Index), true)
-	// 	pokedex.WriteStructToFile(m.Metadata, pokedex.MetadataFpath(metadataDirPath, m.Index))
-	// 	pbar.Add(1)
-	// }
-	// fmt.Println("- Writing total metadata to file")
-	// pokedex.WriteIntToFile(len(metadata), totalFpath)
+	fmt.Println("- Writing metadata and entries to file")
+	pbar := bin.NewProgressBar(len(cowfileFpaths))
+	for i, fpath := range cowfileFpaths {
+		data, err := os.ReadFile(fpath)
+		pokedex.Check(err)
+
+		pokedex.WriteBytesToFile(data, pokedex.EntryFpath(entryDirPath, i), true)
+		pbar.Add(1)
+	}
+	fmt.Println("- Writing total metadata to file")
+	pokedex.WriteIntToFile(len(pokemonMetadata), totalFpath)
 
 	fmt.Println("✓ Complete! Indexed", len(cowfileFpaths), "total cowfiles")
 	fmt.Println("✓ Wrote gzipped metadata to", metadataDirPath)
