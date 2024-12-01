110c0f154aab9ddf7cefdf3e453df91241dddc0e
Tom McKeesick <tmck01@gmail.com>
2024-12-01 22:16:12 +1100

    experiment using channels

--- src/pokesay/print.go
+++ src/pokesay/print.go
@@ -79,6 +79,9 @@ var (
 		"♀": true,
 		"♂": true,
 	}
+	// a sentinel value that won't collide with a regular string (hopefully)
+	// TODO: find a better way to do this
+	Sentinel string = "\x00"
 )
 
 func DetermineBoxChars(unicodeBox bool) *BoxChars {
@@ -96,23 +99,33 @@ func DetermineBoxChars(unicodeBox bool) *BoxChars {
 // 3. The pokemon is printed along with the name & category information
 func Print(args Args, choice int, names []string, categories []string, cows embed.FS) {
 	var b strings.Builder
+	c := make(chan string)
 
 	// pass the buffer to the functions
-	drawSpeechBubble(args.BoxChars, bufio.NewScanner(os.Stdin), args, &b)
-	drawPokemon(args, choice, names, categories, cows, &b)
+	go drawPokemon(args, choice, names, categories, cows, c)
+	go drawSpeechBubble(args.BoxChars, bufio.NewScanner(os.Stdin), args, c)
+
+	sentinels := 0
+	for s := range c {
+		if s == Sentinel {
+			sentinels++
+			if sentinels == 2 {
+				close(c)
+				break
+			}
+		}
+		b.WriteString(s)
+	}
 	fmt.Print(b.String())
 }
 
 // Prints text from STDIN, surrounded by a speech bubble.
-func drawSpeechBubble(boxChars *BoxChars, scanner *bufio.Scanner, args Args, b *strings.Builder) {
+func drawSpeechBubble(boxChars *BoxChars, scanner *bufio.Scanner, args Args, c chan string) {
 	if args.DrawBubble {
-		fmt.Fprintf(
-			b,
-			"%s%s%s\n",
-			boxChars.TopLeftCorner,
-			strings.Repeat(boxChars.HorizontalEdge, args.Width+2),
-			boxChars.TopRightCorner,
-		)
+		// c <- boxChars.TopLeftCorner
+		// c <- strings.Repeat(boxChars.HorizontalEdge, args.Width+2)
+		// c <- boxChars.TopRightCorner + "\n"
+		c <- boxChars.TopLeftCorner + strings.Repeat(boxChars.HorizontalEdge, args.Width+2) + boxChars.TopRightCorner + "\n"
 	}
 
 	for scanner.Scan() {
@@ -122,9 +135,9 @@ func drawSpeechBubble(boxChars *BoxChars, scanner *bufio.Scanner, args Args, b *
 			line = strings.Replace(line, "\t", args.TabSpaces, -1)
 		}
 		if args.NoWrap {
-			drawSpeechBubbleLine(boxChars, line, args, b)
+			drawSpeechBubbleLine(boxChars, line, args, c)
 		} else {
-			drawWrappedText(boxChars, line, args, b)
+			drawWrappedText(boxChars, line, args, c)
 		}
 	}
 
@@ -133,48 +146,51 @@ func drawSpeechBubble(boxChars *BoxChars, scanner *bufio.Scanner, args Args, b *
 		strings.Repeat(boxChars.HorizontalEdge, args.Width+2-7)
 
 	if args.DrawBubble {
-		fmt.Fprintf(b, "%s%s%s\n", boxChars.BottomLeftCorner, bottomBorder, boxChars.BottomRightCorner)
+		// c <- boxChars.BottomLeftCorner
+		// c <- bottomBorder
+		// c <- boxChars.BottomRightCorner + "\n"
+		c <- boxChars.BottomLeftCorner + bottomBorder + boxChars.BottomRightCorner + "\n"
 	} else {
-		fmt.Fprintf(b, " %s \n", bottomBorder)
+		// c <- " "
+		// c <- bottomBorder
+		// c <- " \n"
+		c <- bottomBorder + "\n"
 	}
 	for i := 0; i < 4; i++ {
-		fmt.Fprintf(b, "%s%s\n", strings.Repeat(" ", i+8), boxChars.BalloonString)
+		// c <- strings.Repeat(" ", i+8)
+		// c <- boxChars.BalloonString + "\n"
+		c <- strings.Repeat(" ", i+8) + boxChars.BalloonString + "\n"
 	}
+	c <- Sentinel
 }
 
 // Prints a single speech bubble line
-func drawSpeechBubbleLine(boxChars *BoxChars, line string, args Args, b *strings.Builder) {
+func drawSpeechBubbleLine(boxChars *BoxChars, line string, args Args, c chan string) {
 	if !args.DrawBubble {
-		fmt.Fprintln(b, line)
+		c <- line + "\n"
 		return
 	}
 
 	lineLen := UnicodeStringLength(line)
 	if lineLen <= args.Width {
-		// print the line with padding, the most common case
-		fmt.Fprintf(
-			b,
-			"%s %s%s%s %s\n",
-			boxChars.VerticalEdge, // left-hand side of the bubble
-			line, resetColourANSI, // the text
-			strings.Repeat(" ", args.Width-lineLen), // padding
-			boxChars.VerticalEdge,                   // right-hand side of the bubble
-		)
+		// c <- boxChars.VerticalEdge
+		// c <- line
+		// c <- resetColourANSI
+		// c <- strings.Repeat(" ", args.Width-lineLen)
+		// c <- boxChars.VerticalEdge + "\n"
+		c <- boxChars.VerticalEdge + line + resetColourANSI + strings.Repeat(" ", args.Width-lineLen) + boxChars.VerticalEdge + "\n"
 	} else if lineLen > args.Width {
-		// print the line without padding or right-hand side of the bubble if the line is too long
-		fmt.Fprintf(
-			b,
-			"%s %s%s\n",
-			boxChars.VerticalEdge, // left-hand side of the bubble
-			line, resetColourANSI, // the text
-		)
+		// c <- boxChars.VerticalEdge
+		// c <- line
+		// c <- resetColourANSI + "\n"
+		c <- boxChars.VerticalEdge + line + resetColourANSI + "\n"
 	}
 }
 
 // Prints line of text across multiple lines, wrapping it so that it doesn't exceed the desired width.
-func drawWrappedText(boxChars *BoxChars, line string, args Args, b *strings.Builder) {
+func drawWrappedText(boxChars *BoxChars, line string, args Args, c chan string) {
 	for _, wline := range strings.Split(wordwrap.WrapString(strings.Replace(line, "\t", args.TabSpaces, -1), uint(args.Width)), "\n") {
-		drawSpeechBubbleLine(boxChars, wline, args, b)
+		drawSpeechBubbleLine(boxChars, wline, args, c)
 	}
 }
 
@@ -226,8 +242,9 @@ func UnicodeStringLength(s string) int {
 }
 
 // Prints a pokemon with its name & category information.
-func drawPokemon(args Args, index int, names []string, categoryKeys []string, GOBCowData embed.FS, b *strings.Builder) {
+func drawPokemon(args Args, index int, names []string, categoryKeys []string, GOBCowData embed.FS, c chan string) {
 	d, _ := GOBCowData.ReadFile(pokedex.EntryFpath("build/assets/cows", index))
+	c <- string(pokedex.Decompress(d))
 
 	width := nameLength(names)
 	namesFmt := make([]string, 0)
@@ -239,21 +256,43 @@ func drawPokemon(args Args, index int, names []string, categoryKeys []string, GO
 	width += 2     // for the arrow
 	width += 2 + 2 // for the end box characters
 
-	infoLine := ""
+	if args.DrawInfoBorder {
+		// topBorder := fmt.Sprintf(
+		// 	"%s%s%s",
+		// 	args.BoxChars.TopLeftCorner, strings.Repeat(args.BoxChars.HorizontalEdge, width-2), args.BoxChars.TopRightCorner,
+		// )
+		c <- args.BoxChars.TopLeftCorner
+		c <- strings.Repeat(args.BoxChars.HorizontalEdge, width-2)
+		c <- args.BoxChars.TopRightCorner + "\n"
+		// c <- args.BoxChars.TopLeftCorner + strings.Repeat(args.BoxChars.HorizontalEdge, width-2) + args.BoxChars.TopRightCorner + "\n"
+	}
 
+	infoSep := " " + args.BoxChars.Separator + " "
+	if args.DrawInfoBorder {
+		c <- args.BoxChars.VerticalEdge + " "
+	}
 	if args.NoCategoryInfo {
-		infoLine = fmt.Sprintf(
-			"%s %s",
-			args.BoxChars.RightArrow, strings.Join(namesFmt, fmt.Sprintf(" %s ", args.BoxChars.Separator)),
-		)
+		// infoLine = fmt.Sprintf(
+		// 	"%s %s",
+		// 	args.BoxChars.RightArrow, strings.Join(namesFmt, fmt.Sprintf(" %s ", args.BoxChars.Separator)),
+		// )
+		// c <- args.BoxChars.RightArrow
+		// c <- strings.Join(namesFmt, infoSep) + "\n"
+		c <- args.BoxChars.RightArrow + strings.Join(namesFmt, infoSep) + "\n"
 	} else {
-		infoLine = fmt.Sprintf(
-			"%s %s %s %s",
-			args.BoxChars.RightArrow,
-			strings.Join(namesFmt, fmt.Sprintf(" %s ", args.BoxChars.Separator)),
-			args.BoxChars.Separator,
-			textStyleItalic.Sprint(strings.Join(categoryKeys, args.BoxChars.CategorySeparator)),
-		)
+		// infoLine = fmt.Sprintf(
+		// 	"%s %s %s %s",
+		// 	args.BoxChars.RightArrow,
+		// 	strings.Join(namesFmt, fmt.Sprintf(" %s ", args.BoxChars.Separator)),
+		// 	args.BoxChars.Separator,
+		// 	textStyleItalic.Sprint(strings.Join(categoryKeys, args.BoxChars.CategorySeparator)),
+		// )
+		// c <- args.BoxChars.RightArrow
+		// c <- strings.Join(namesFmt, infoSep)
+		// c <- infoSep
+		// c <- textStyleItalic.Sprint(strings.Join(categoryKeys, args.BoxChars.CategorySeparator)) + "\n"
+		c <- args.BoxChars.RightArrow + strings.Join(namesFmt, infoSep) + infoSep + textStyleItalic.Sprint(strings.Join(categoryKeys, args.BoxChars.CategorySeparator)) + "\n"
+
 		for _, category := range categoryKeys {
 			width += len(category)
 		}
@@ -261,20 +300,13 @@ func drawPokemon(args Args, index int, names []string, categoryKeys []string, GO
 	}
 
 	if args.DrawInfoBorder {
-		topBorder := fmt.Sprintf(
-			"%s%s%s",
-			args.BoxChars.TopLeftCorner, strings.Repeat(args.BoxChars.HorizontalEdge, width-2), args.BoxChars.TopRightCorner,
-		)
-		bottomBorder := fmt.Sprintf(
-			"%s%s%s",
-			args.BoxChars.BottomLeftCorner, strings.Repeat(args.BoxChars.HorizontalEdge, width-2), args.BoxChars.BottomRightCorner,
-		)
-		infoLine = fmt.Sprintf(
-			"%s\n%s %s %s\n%s\n",
-			topBorder, args.BoxChars.VerticalEdge, infoLine, args.BoxChars.VerticalEdge, bottomBorder,
-		)
-	} else {
-		infoLine = fmt.Sprintf("%s\n", infoLine)
+		// c <- " " + args.BoxChars.VerticalEdge + "\n"
+		// c <- args.BoxChars.BottomLeftCorner
+		// c <- strings.Repeat(args.BoxChars.HorizontalEdge, width-2)
+		// c <- args.BoxChars.BottomRightCorner + "\n"
+		c <- " " + args.BoxChars.VerticalEdge + "\n" + args.BoxChars.BottomLeftCorner + strings.Repeat(args.BoxChars.HorizontalEdge, width-2) + args.BoxChars.BottomRightCorner + "\n"
 	}
-	fmt.Fprintf(b, "%s%s", pokedex.Decompress(d), infoLine)
+
+	c <- Sentinel
+	// close(c)
 }
