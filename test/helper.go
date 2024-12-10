b9131a7d670b4fdd3a519e634c308e65e3bc83b4
Tom McKeesick <tmck01@gmail.com>
2024-12-10 23:34:43 +1100

    colorise test output
    green for yay, red for nay

--- test/helper.go
+++ test/helper.go
@@ -22,7 +22,7 @@ func debug() bool {
 
 // Fails a test with a formatted message showing the expected vs. result. (These are both printed in %#v form)
 func Fail(expected interface{}, result interface{}, test *testing.T) {
-	test.Fatalf("%s items don't match!\n> expected:\t%#v\x1b[0m\n>   result:\t%#v\x1b[0m\n", failMark, expected, result)
+	test.Fatalf("\n\x1b[38;5;196m%s items don't match!\x1b[0m\n> expected:\t%#v\x1b[0m\n>   result:\t%#v\x1b[0m\n", failMark, expected, result)
 }
 
 // Takes in an expected & result object, of any type.
@@ -33,7 +33,7 @@ func Assert(expected interface{}, result interface{}, test *testing.T) {
 	expectedString, resultString := fmt.Sprintf("%#v", expected), fmt.Sprintf("%#v", result)
 	if expectedString == resultString {
 		if debug() {
-			fmt.Printf("%s items match!\n> expected:\t%s\x1b[0m\n>   result:\t%s\x1b[0m\n", successMark, expected, result)
+			fmt.Printf("\x1b[38;5;46m%s items match!\x1b[0m\n> expected:\t%s\x1b[0m\n>   result:\t%s\x1b[0m\n", successMark, expected, result)
 		}
 		return
 	}
