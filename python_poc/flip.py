c0d76c2f3e91230ce445cf3b5cca9f4a37cb3f51
Tom McKeesick <tmck01@gmail.com>
2025-06-05 02:27:35 +1000

    neaten POC
    tidy implementation
    add comments
    improve unit tests

--- python_poc/flip.py
+++ python_poc/flip.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python3
 
-import sys
+import os, sys
 from typing import Iterator
 from itertools import chain
+from collections import deque
 
-msg = [
-    # red ▄, green ▄
-    "\033[38;5;160m▄x \033[38;5;46m▄",
-    # green (via continuation)▄, yellow ▄
-    "x▄ \033[38;5;190m▄",
-]
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
 def tokenise_ansi(msg: str) -> Iterator[list[tuple[str, str]]]:
-    is_colour = False
-    colour, colour_bg, colour_fg = '', '', ''
-    tokens = []
-    lines = msg.split('\n')
-    for i in range(len(lines)):
-        line = lines[i]
-        text = ''
-        # print(f'{line=}')
+    colour, colour_fg, colour_bg = '', deque([], 1), deque([], 1)
+    is_colour, is_reeet = False, False
+    for i, line in enumerate(msg.split('\n')):
+        colour, text, tokens = '', '', []
         for ch in line:
+            # start of colour sequence detected!
             if ch == '\033':
                 is_colour = True
+                # if there is text in the current token buffer,
                 if text:
-                    tokens.append((colour_fg+colour_bg, text))
-                    colour = ch
-                    text = ''
+                    # save it,
+                    if DEBUG:
+                        print(f'> {text=}, {colour_fg=}, {colour_bg=}, {colour=}, {is_colour=}')
+                    tokens.append((''.join([*colour_fg, *colour_bg]), text))
+                    colour = ch # then start a new colour token (unknown if bg/fg)
+                    text = ''   # & clear the text buffer
+
+                    # Do NOT clear the colour_fg/bg variables!
+                    # This allows the next line to continue with the same colour.
+
+                # or, if the text buffer is empty,
                 else:
-                    colour += ch
+                    colour = ch # keep appending to the current colour token
+            # middle of colour sequence
             elif is_colour:
+                # append to current colour token
                 colour += ch
+                # check if we're at the end of a colour sequence
                 if ch == 'm':
+                    # if so, set the colour detection to false
                     is_colour = False
+                    # check if the current colour token is fg or bg
                     if '[38' in colour or '[39' in colour:
-                        colour_fg = colour
+                        colour_fg.append(colour)
                     elif '[48' in colour or '[49' in colour:
-                        colour_bg = colour
+                        colour_bg.append(colour)
+                    elif '[0m' in colour:
+                        # if we reach the end of a colour sequence, reset colours
+                        is_reset = True
+                        colour = ''
+            # othwise, if we're not in a colour sequence
             else:
-                text += ch
+                text += ch # append to the current text buffer
+        # if we reach the end of a line, we need to flush the text buffer
         if colour:
-            tokens.append((colour_fg+colour_bg, text))
-            colour = ''
-        # if i < len(lines) - 1:
-        #     if lines[i+1].strip().startswith('\x1b[49m'):
-        #         tokens.append(('\x1b[49m', ''))  # reset background colour
-        #     elif lines[i+1].strip().startswith('\x1b[0m'):
-        #         tokens.append(('\x1b[0m', ''))
-        #     elif lines[i+1].strip().startswith('\x1b[39m'):
-        #         tokens.append(('\x1b[39m', ''))
+            if DEBUG:
+                print(f'! {text=}, {colour_fg=}, {colour_bg=}, {colour=}, {is_colour=}\n')
+            tokens.append((''.join([*colour_fg, *colour_bg]), text))
         yield tokens
         tokens = []
 
@@ -70,39 +77,25 @@ def reverse_ansi(msg: str) -> Iterator[str]:
         length = line_len(line)
         rev_line = [('', ' ' * (max_len - length))]
         for colour, text in reversed(line):
-            rev = (colour, text[::-1])
-            # print(f'{rev=}')
-            rev_line += (colour, text[::-1])
+            rev_line.append((colour, text[::-1]))
         if set(rev_line[-1][1]) == {' '}:
             rev_line[-1] = (rev_line[-1][0] + '\x1b[49m', rev_line[-1][1])
 
-        yield ' '*4 + ''.join(chain.from_iterable(rev_line))
+        # yield ' '*4 + ''.join(chain.from_iterable(rev_line))
+        yield rev_line
 
 def print_reversed_ansi(msg: str) -> None:
-    print('original:')
-    print(msg)
-    print('\033[0m')
+    print('original:', msg, '\x1b[0m', sep='\n')
 
     print('scanned:')
-    max_len = max_line_len(msg)
     for line in tokenise_ansi(msg):
         print(''.join(chain.from_iterable(line)), end='\x1b[0m\n')
 
     print('reversed:')
     for rev_line in reverse_ansi(msg):
-        print(rev_line, end='\x1b[0m\n')
 
-
-
-# t = list(tokenise_ansi('\n'.join(msg)))
-# print('\n'.join(msg))
-# print('\033[0m')
-#
-# for line in t:
-#     print(f'\033[0m{line=}')
-# print()
-#
-# print_reversed_ansi('\n'.join(msg))
+        line = ' '*4 + ''.join(chain.from_iterable(rev_line))
+        print(line, end='\x1b[0m\n')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
