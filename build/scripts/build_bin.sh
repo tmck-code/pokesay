90fac3f25636355e1da3fa04fc6ffbdcf0790d2c
Tom McKeesick <tmck01@gmail.com>
2025-10-03 00:44:01 +1000

    write all builds to dist/
    - binaries are now versioned like the OS packages
    - added tarballs that contain the binary, LICENSE and manpage
    - write all binaries, OS packages & tarballs to dist/, i.e.
    
    ../dist/
    ├── bin
    │   ├── pokesay-0.18.1-android-arm64
    │   ├── pokesay-0.18.1-darwin-amd64
    │   ├── pokesay-0.18.1-darwin-arm64
    │   ├── pokesay-0.18.1-linux-amd64
    │   └── pokesay-0.18.1-windows-amd64.exe
    ├── packages
    │   ├── pokesay-0.18.1-1-x86_64.pkg.tar.zst
    │   ├── pokesay-0.18.1-android-arm64.deb
    │   ├── pokesay-0.18.1-linux-amd64.deb
    │   └── pokesay-debug-0.18.1-1-x86_64.pkg.tar.zst
    └── tarballs
        └── pokesay-0.18.1-linux-amd64.tar.gz
    
    This now requires running the build task with a VERSION:
    VERSION=0.18.1 make build/bin build/packages
    
    These files can then all be uploaded as part of a release.
    
    Then, afterward, the binaries/tarballs can be referenced in AUR and
    homebrew packages.

--- build/scripts/build_bin.sh
+++ build/scripts/build_bin.sh
@@ -2,13 +2,28 @@
 
 set -euo pipefail
 
-OUTPUT_DIR="build/bin"
+[ -z "${VERSION:-}" ] && exit 1
+
+OUTPUT_DIR="dist/bin"
 mkdir -p "$OUTPUT_DIR"
 
+cp build/packages/pokesay.1 .
+
 function build() {
-    echo "building $1 / $2"
-    GOOS=$1 GOARCH=$2 go build -o "${OUTPUT_DIR}/pokesay-${1}-${2}${3:-}" pokesay.go
-    echo "- built as ${OUTPUT_DIR}/pokesay-${1}-${2}${3:-}"
+    echo "- building $1 / $2"
+    GOOS=$1 GOARCH=$2 go build -o "${OUTPUT_DIR}/pokesay-${VERSION}-${1}-${2}${3:-}" pokesay.go
+    echo "- built as ${OUTPUT_DIR}/pokesay-${VERSION}-${1}-${2}${3:-}"
+}
+
+function tarball() {
+    echo "- tarballing $1 / $2"
+    cp "${OUTPUT_DIR}/pokesay-${VERSION}-${1}-${2}${3:-}" .
+
+    tar czf \
+        "dist/tarballs/pokesay-${VERSION}-${1}-${2}${3:-}.tar.gz" \
+        "pokesay-${VERSION}-${1}-${2}${3:-}" LICENSE pokesay.1
+
+    rm -f "pokesay-${VERSION}-${1}-${2}${3:-}"
 }
 
 build darwin  amd64 &
@@ -17,3 +32,8 @@ build linux   amd64 &
 build windows amd64 .exe &
 build android arm64 &
 wait
+
+# just create a tarball for the linux/amd64 (used for AUR package)
+tarball linux amd64
+
+rm -f pokesay.1
