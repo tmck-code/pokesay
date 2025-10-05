9ef22d7181de0ce2f146d763a358c693898c7a66
Tom McKeesick <tmck01@gmail.com>
2025-10-06 00:18:03 +1100

    update completions
    - short and long opts are now listed beside each other
    - add descriptions for every opt

--- build/packages/pokesay-completion.bash
+++ build/packages/pokesay-completion.bash
@@ -7,10 +7,27 @@ _pokesay_completions()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     local opts=(
-        --help -h --verbose -v --name -n --id -i --category -c --list-names -l --list-categories -L --width -w --tab-width -t --no-wrap -W --no-tab-spaces -s --fastest -f --no-bubble -B --japanese-name -j --id-info -I --no-category-info -C --info-border -b --unicode-borders -u --flip -F completion
+        -h --help
+        -v --verbose
+        -n --name
+        -i --id
+        -c --category
+        -l --list-names
+        -L --list-categories
+        -w --width
+        -t --tab-width
+        -W --no-wrap
+        -s --no-tab-spaces
+        -f --fastest
+        -B --no-bubble
+        -j --japanese-name
+        -I --id-info
+        -C --no-category-info
+        -b --info-border
+        -u --unicode-borders
+        -F --flip
     )
 
-    # Read names and ids from external files
     names="$(</usr/share/pokesay/pokesay-names.txt)"
     ids="$(</usr/share/pokesay/pokesay-ids.txt)"
     cats="big female gen7x gen8 medium regular right shiny small"
@@ -18,19 +35,13 @@ _pokesay_completions()
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts[*]}" -- ${cur}) )
         return 0
-    fi
-
-    if [[ ${prev} == "--category" || ${prev} == "-c" ]]; then
+    elif [[ ${prev} == "--category" || ${prev} == "-c" ]]; then
         COMPREPLY=( $(compgen -W "${cats}" -- ${cur}) )
         return 0
-    fi
-
-    if [[ ${prev} == "--name" || ${prev} == "-n" ]]; then
+    elif [[ ${prev} == "--name" || ${prev} == "-n" ]]; then
         COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
         return 0
-    fi
-
-    if [[ ${prev} == "--id" || ${prev} == "-i" ]]; then
+    elif [[ ${prev} == "--id" || ${prev} == "-i" ]]; then
         COMPREPLY=( $(compgen -W "${ids}" -- ${cur}) )
         return 0
     fi
--- build/packages/pokesay-completion.zsh
+++ build/packages/pokesay-completion.zsh
@@ -2,49 +2,56 @@
 
 _pokesay() {
   local -a opts names ids cats
+  # Option descriptions from README Full Usage
+  # e.g. 
+  # '-T+[Defines the window title \[default\: Alacritty\]]:TITLE:_default' \
+  # '--title=[Defines the window title \[default\: Alacritty\]]:TITLE:_default' \
   opts=(
-    --help -h --verbose -v --name -n --id -i --category -c --list-names -l --list-categories -L --width -w --tab-width -t --no-wrap -W --no-tab-spaces -s --fastest -f --no-bubble -B --japanese-name -j --id-info -I --no-category-info -C --info-border -b --unicode-borders -u --flip -F completion
+    '-h[Display this help message]:HELP'
+    '--help[Display this help message]:HELP'
+    '-v[Print verbose output]:VERBOSE'
+    '--verbose[Print verbose output]:VERBOSE'
+    '-n=[Choose a Pokémon from a specific name]:NAME:(${names[*]})'
+    '--name=[Choose a Pokémon from a specific name]:NAME:(${names[*]})'
+    '-i=[Choose a Pokémon from a specific ID]:ID:(${ids[*]})'
+    '--id=[Choose a Pokémon from a specific ID]:ID:(${ids[*]})'
+    '-c=[Choose a Pokémon from a specific category]:CATEGORY:(${cats[*]})'
+    '--category=[Choose a Pokémon from a specific category]:CATEGORY:(${cats[*]})'
+    '-l[List all available names]:LIST_NAMES'
+    '--list-names[List all available names]:LIST_NAMES'
+    '-L[List all available categories]:LIST_CATEGORIES'
+    '--list-categories[List all available categories]:LIST_CATEGORIES'
+    '-w=[Set max speech bubble width \[default\: 80\]]:WIDTH'
+    '--width=[Set max speech bubble width \[default\: 80\]]:WIDTH'
+    '-t=[Replace tab characters with N spaces \[default\: 4\]]:TAB-WIDTH'
+    '--tab-width=[Replace tab characters with N spaces \[default\: 4\]]:TAB-WIDTH'
+    '-W[Disable text wrapping (fastest)]:DISABLE_WRAP'
+    '--no-wrap[Disable text wrapping (fastest)]:DISABLE_WRAP'
+    '-s[Do not replace tab characters (fastest)]:DISABLE_TAB_SPACES'
+    '--no-tab-spaces[Do not replace tab characters (fastest)]:DISABLE_TAB_SPACES'
+    '-f[Run with the fastest possible configuration (--nowrap & --notabspaces)]:FASTEST'
+    '--fastest[Run with the fastest possible configuration (--nowrap & --notabspaces)]:FASTEST'
+    '-B[Do not draw the speech bubble]:DISABLE_BUBBLE'
+    '--no-bubble[Do not draw the speech bubble]:DISABLE_BUBBLE'
+    '-j[Print the Japanese name in the info box]:JAPANESE_NAME'
+    '--japanese-name[Print the Japanese name in the info box]:JAPANESE_NAME'
+    '-I[Print the Pokémon ID in the info box]:POKEMON_ID'
+    '--id-info[Print the Pokémon ID in the info box]:POKEMON_ID'
+    '-C[Do not print category info in the info box]:DISABLE_CATEGORY_INFO'
+    '--no-category-info[Do not print category info in the info box]:DISABLE_CATEGORY_INFO'
+    '-b[Draw a border around the info box]:INFO_BORDER'
+    '--info-border[Draw a border around the info box]:INFO_BORDER'
+    '-u[Use unicode characters to draw the border]:UNICODE_BORDERS'
+    '--unicode-borders[Use unicode characters to draw the border]:UNICODE_BORDERS'
+    '-F[Flip the Pokémon horizontally (face right instead of left)]:FLIP'
+    '--flip[Flip the Pokémon horizontally (face right instead of left)]:FLIP'
   )
   names=($(</usr/share/pokesay/pokesay-names.txt))
   ids=($(</usr/share/pokesay/pokesay-ids.txt))
   cats=(big female gen7x gen8 medium regular right shiny small)
 
   _arguments \
-    "${opts[@]/#/-}" \
-    '--name[Choose a Pokémon name]:name:(( ${names[*]} ))' \
-    '--id[Choose a Pokémon ID]:id:(( ${ids[*]} ))' \
-    '--category[Choose a category]:category:(( ${cats[*]} ))' \
-    '--list-names[List all available names]' \
-    '--list-categories[List all available categories]' \
-    '--width[Set max speech bubble width]' \
-    '--tab-width[Set tab width]' \
-    '--no-wrap[Disable text wrapping]' \
-    '--no-tab-spaces[Do not replace tab characters]' \
-    '--fastest[Fastest configuration]' \
-    '--no-bubble[Do not draw speech bubble]' \
-    '--japanese-name[Print Japanese name]' \
-    '--id-info[Print Pokémon ID]' \
-    '--no-category-info[Do not print category info]' \
-    '--info-border[Draw info box border]' \
-    '--unicode-borders[Use unicode borders]' \
-    '--flip[Flip Pokémon]' \
-    '-n[Choose a Pokémon name]:name:(( ${names[*]} ))' \
-    '-i[Choose a Pokémon ID]:id:(( ${ids[*]} ))' \
-    '-c[Choose a category]:category:(( ${cats[*]} ))' \
-    '-l[List all available names]' \
-    '-L[List all available categories]' \
-    '-w[Set max speech bubble width]' \
-    '-t[Set tab width]' \
-    '-W[Disable text wrapping]' \
-    '-s[Do not replace tab characters]' \
-    '-f[Fastest configuration]' \
-    '-B[Do not draw speech bubble]' \
-    '-j[Print Japanese name]' \
-    '-I[Print Pokémon ID]' \
-    '-C[Do not print category info]' \
-    '-b[Draw info box border]' \
-    '-u[Use unicode borders]' \
-    '-F[Flip Pokémon]'
+    ${opts[@]}
 }
 
 compdef _pokesay pokesay
