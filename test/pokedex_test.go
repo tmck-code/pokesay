c4b2d5510199bb68eeb18eea430f9a9e0b896323
Tom McKeesick <tmck01@gmail.com>
2025-11-12 17:46:04 +1100

    add test for similar names

--- test/pokedex_test.go
+++ test/pokedex_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"embed"
+	"fmt"
 	"os"
 	"testing"
 
@@ -48,6 +49,78 @@ func TestReadMetadataFromEmbedded(test *testing.T) {
 			{EntryIndex: 2960, Categories: []string{"small", "gen8", "regular"}},
 			{EntryIndex: 4285, Categories: []string{"small", "gen8", "shiny"}},
 			{EntryIndex: 428, Categories: []string{"small", "gen7x", "regular"}},
+		},
+	}
+
+	Assert(expected, result, test)
+}
+
+func TestCreateNameMetadataMew(test *testing.T) {
+	result := pokedex.CreateNameMetadata(
+		fmt.Sprintf("%04d", 0),
+		"mew",
+		pokedex.PokemonName{English: "Mew", Japanese: "ミュウ", JapanesePhonetic: "myuu", Slug: "mew"},
+		"data/cows/similar_names/",
+		[]string{"data/cows/similar_names/gen7x/mew.cow", "data/cows/similar_names/gen7x/mewtwo.cow"},
+	)
+	expected := &pokedex.PokemonMetadata{
+		Idx:              "0000",
+		Name:             "Mew",
+		JapaneseName:     "ミュウ",
+		JapanesePhonetic: "myuu",
+		Entries: []pokedex.PokemonEntryMapping{
+			{EntryIndex: 0, Categories: []string{"medium", "gen7x"}},
+		},
+	}
+
+	Assert(expected, result, test)
+}
+
+func TestCreateNameMetadataNatu(test *testing.T) {
+	result := pokedex.CreateNameMetadata(
+		fmt.Sprintf("%04d", 0),
+		"natu",
+		pokedex.PokemonName{English: "Natu", Japanese: "ネイティ", JapanesePhonetic: "neiti", Slug: "natu"},
+		"data/cows/similar_names/",
+		[]string{
+			"data/cows/similar_names/gen8/natu.cow",
+			"data/cows/similar_names/gen8/eternatus.cow",
+			"data/cows/similar_names/gen8/eternatus-eternamax.cow",
+		},
+	)
+	expected := &pokedex.PokemonMetadata{
+		Idx:              "0000",
+		Name:             "Natu",
+		JapaneseName:     "ネイティ",
+		JapanesePhonetic: "neiti",
+		Entries: []pokedex.PokemonEntryMapping{
+			{EntryIndex: 0, Categories: []string{"small", "gen8"}},
+		},
+	}
+
+	Assert(expected, result, test)
+}
+
+func TestCreateNameMetadataEternatus(test *testing.T) {
+	result := pokedex.CreateNameMetadata(
+		fmt.Sprintf("%04d", 0),
+		"eternatus",
+		pokedex.PokemonName{English: "Eternatus", Japanese: "ムゲンダイナ", JapanesePhonetic: "mugendaina", Slug: "eternatus"},
+		"data/cows/similar_names/",
+		[]string{
+			"data/cows/similar_names/gen8/natu.cow",
+			"data/cows/similar_names/gen8/eternatus.cow",
+			"data/cows/similar_names/gen8/eternatus-eternamax.cow",
+		},
+	)
+	expected := &pokedex.PokemonMetadata{
+		Idx:              "0000",
+		Name:             "Eternatus",
+		JapaneseName:     "ムゲンダイナ",
+		JapanesePhonetic: "mugendaina",
+		Entries: []pokedex.PokemonEntryMapping{
+			{EntryIndex: 1, Categories: []string{"big", "gen8"}},
+			{EntryIndex: 2, Categories: []string{"big", "gen8"}},
 		},
 	}
 
