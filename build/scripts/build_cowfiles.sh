f41d3b39c7be9f31759463dc5d718e4b2202e12e
Tom McKeesick <tmck01@gmail.com>
2025-12-05 10:27:44 +1100

    modify cow build script
    - just detect skip dirs & duplicate files
    - output to build/cows

--- build/scripts/build_cowfiles.sh
+++ build/scripts/build_cowfiles.sh
@@ -2,16 +2,39 @@
 
 set -euo pipefail
 
-mkdir -p /tmp/convert/
+skip=(resources/ misc/ icons/ items/ items-outline)
+declare -A h
 
-# Convert all of the pokesprite .pngs -> cowfiles for the terminal
-go run /usr/local/src/src/bin/convert/png_convert.go \
-  -from /tmp/original/pokesprite/ \
-  -tmpDir /tmp/convert/ \
-  -to /tmp/cows/ \
-  -padding 4 \
-  -skipDuplicates \
-  -skip '["resources/", "misc/", "icons/", "items/", "items-outline/"]' \
-  && mv -v /tmp/cows/pokemon-gen8 /tmp/cows/gen8 > /dev/null \
-  && mv -v /tmp/cows/pokemon-gen7x /tmp/cows/gen7x > /dev/null \
-  && cat /tmp/original/pokesprite/data/pokemon.json | jq -c .[] > /tmp/cows/pokemon.json
+cd /tmp/original/cows/
+fpaths=$(find . -iname '*.cow' -type f | sort -h)
+
+nOldFpaths=$(echo "$fpaths" | wc -l | tr -d '\n')
+i=1
+echo "$fpaths" | while read f; do
+  progress=$(( ((i * 100)) / nOldFpaths))
+  for s in "${skip[@]}"; do
+    if [[ "$f" == *"$s"* ]]; then
+      echo -e "[$i/$nOldFpaths $progress%] \e[0;33m skipping:  $f\e[0m"
+      ((i++))
+      continue 2
+    fi
+  done
+
+  s=$(sha256sum "$f" | cut -d' ' -f1)
+  if [[ ${h[$s]:-} ]]; then
+    echo -e "[$i/$nOldFpaths $progress%] \e[0;31m duplicate: $f\e[0m"
+  else
+    echo -e "[$i/$nOldFpaths $progress%] \e[0;32m copying:   $f > /tmp/cows/$f\e[0m"
+    cp --parents "$f" /tmp/cows/
+    h[$s]=1
+  fi
+  ((i++))
+done
+
+# rename dirs & generate pokemon.json metadata file
+mv -v /tmp/cows/pokemon-gen8 /tmp/cows/gen8
+mv -v /tmp/cows/pokemon-gen7x /tmp/cows/gen7x
+cat /tmp/original/cows/data/pokemon.json | jq -c .[] > /tmp/cows/pokemon.json
+
+nNewFpaths=$(find /tmp/cows/ -iname '*.cow' | wc -l | tr -d '\n')
+echo -e "\n\e[1;32m✔ all done, total files: $nNewFpaths / $nOldFpaths\e[0m"
